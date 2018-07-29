0d4a781379b198d5ccf0084ff67d2d87164e0428
1144388620lq <1144388620@qq.com>
2018-07-29 18:11:49 +0800

    前端代码 以及和后端的整合 调试等

---
+++ src/main/java/com/lq/sell/MyWebMvcConfigurerAdapter.java
@@ -0,0 +1,15 @@
+package com.lq.sell;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/index/**").addResourceLocations("classpath:");
+
+        super.addResourceHandlers(registry);
+    }
+}
